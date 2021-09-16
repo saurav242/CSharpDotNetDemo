e965108cfa62b317340ab8ef7f77f2e94cd2651c
Saurav Kumar <Saurav.Kumar3@mindtree.com>
2021-09-16 12:56:23 +0530

    commits

--- CSharpDotNetDemo.SQL/Table1.sql
+++ CSharpDotNetDemo.SQL/Table1.sql
@@ -1,4 +1,17 @@
-﻿CREATE TABLE [dbo].[Table1]
+﻿CREATE TABLE Employee
 (
-	[Id] INT NOT NULL PRIMARY KEY
+    Id INT NOT NULL IDENTITY,
+    Email VARCHAR(50),
+    UserName VARCHAR(100) ,
+    DeptId INT NOT NULL,
+    PRIMARY KEY (Id, Email),
+    UNIQUE(UserName, Email)
 )
+
+CREATE TABLE Department
+{
+    Id INT NOT NULL PRIMARY KEY IDENTITY,
+    Name VARCHAR(100),
+
+
+}
